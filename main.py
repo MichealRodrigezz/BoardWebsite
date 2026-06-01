6198d25242e2e7be2f5bad1fb078024875298c36
MichealRodrigezz <your_github_email@example.com>
2026-06-01 15:35:56 +0530

    My website

---
+++ main.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/timetable")
+def timetable():
+    return render_template("timetable.html")
+
+@app.route("/subjects")
+def subjects():
+    return render_template("subjects.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+@app.route("/topper")
+def topper():
+    return render_template("topper.html")
+
+@app.route("/motivation")
+def motivation():
+    return render_template("motivation.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        name = request.form["name"]
+
+        return f"Registration Successful! Welcome {name}"
+
+    return render_template("register.html")
+
+app.run(debug=True)
